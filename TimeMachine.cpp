f30ed7cb9c140f4cfda52a23593c54fe38cac336
kayla-kubicke <161649341+kayla-kubicke@users.noreply.github.com>
2025-09-29 12:55:58 -0700

    Restarting Project
    Pulling new branch to work on default constructor.

--- TimeMachine.cpp
+++ TimeMachine.cpp
@@ -1,11 +1,2 @@
 // @file TimeMachine.cpp
 // Kayla Kubicke, CSS 342, TimeSpan
-
-//
-int main()
-{
-	// Note: This is not required.
-	// The C++ Programming Language, Stroustrup:
-	// "If no value is returned, the system will receive a value indicating successful completion."
-	return 0;
-}
